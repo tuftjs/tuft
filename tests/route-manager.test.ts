10bf97a409d9cf2a67daff73dabc6c6e79aa514e
Stuart Kennedy <rav2040@gmail.com>
2020-04-12 14:36:19 +1200

    Update tests

--- tests/route-manager.test.ts
+++ tests/route-manager.test.ts
@@ -1,4 +1,4 @@
-import { RouteManager } from '../src/route-manager';
+import { RouteManager, RouteStore } from '../src/route-manager';
 
 const mockRouteMap = new Map();
 
@@ -35,6 +35,127 @@ describe('RouteManager', () => {
     test('returns a function', () => {
       const route = routes.find('GET', '/foo');
       expect(typeof route).toBe('function');
+    });
+  });
+});
+
+describe('RouteStore', () => {
+  const store = new RouteStore();
+
+  describe('RouteStore.prototype.set()', () => {
+    describe('with an argument of \'/foo\'', () => {
+      test('does not throw an error', () => {
+        function setRoute() {
+          store.set('/foo', {
+            preHandlers: [],
+            response: {},
+          },);
+        }
+
+        expect(setRoute).not.toThrow();
+      });
+    });
+
+    describe('with an argument of \'/foo/{bar}\'', () => {
+      test('does not throw an error', () => {
+        function setRoute() {
+          store.set('/foo/{bar}', {
+            preHandlers: [],
+            response: {},
+          });
+        }
+
+        expect(setRoute).not.toThrow();
+      });
+    });
+
+    describe('with an argument of \'/foo/bar/{*}\'', () => {
+      test('does not throw an error', () => {
+        function setRoute() {
+          store.set('/foo/bar/{*}', {
+            preHandlers: [],
+            response: {},
+          });
+        }
+
+        expect(setRoute).not.toThrow();
+      });
+    });
+
+    describe('with an argument of \'/{**}\'', () => {
+      test('does not throw an error', () => {
+        function setRoute() {
+          store.set('/{**}', {
+            preHandlers: [],
+            response: {},
+          });
+        }
+
+        expect(setRoute).not.toThrow();
+      });
+    });
+  });
+});
+
+
+describe('RouteStore', () => {
+  const store = new RouteStore();
+
+  store.set('/foo', {
+    preHandlers: [],
+    response: {},
+  });
+
+  store.set('/foo/{*}', {
+    preHandlers: [],
+    response: {},
+  });
+
+  store.set('/foo/bar/{*}', {
+    preHandlers: [],
+    response: {},
+  });
+
+  store.set('/bar/{**}', {
+    preHandlers: [],
+    response: {},
+  });
+
+  describe('RouteStore.prototype.get()', () => {
+    describe('with an argument of \'/foo\'', () => {
+      test('returns a function', () => {
+        expect(typeof store.get('/foo')).toBe('function');
+      });
+    });
+
+    describe('with an argument of \'/foo/bar\'', () => {
+      test('returns a function', () => {
+        expect(typeof store.get('/foo/bar')).toBe('function');
+      });
+    });
+
+    describe('with an argument of \'/foo/bar/baz\'', () => {
+      test('returns a function', () => {
+        expect(typeof store.get('/foo/bar/baz')).toBe('function');
+      });
+    });
+
+    describe('with an argument of \'/bar/baz\'', () => {
+      test('returns a function', () => {
+        expect(typeof store.get('/bar/baz')).toBe('function');
+      });
+    });
+
+    describe('with an argument of \'/bar/bar/bar\'', () => {
+      test('returns a function', () => {
+        expect(typeof store.get('/bar/bar/bar')).toBe('function');
+      });
+    });
+
+    describe('with an argument of \'/bar\'', () => {
+      test('returns undefined', () => {
+        expect(store.get('/bar')).toBeUndefined();
+      });
     });
   });
 });
