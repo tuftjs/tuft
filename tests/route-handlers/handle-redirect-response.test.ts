3a11c9eefc8b8f5fde1502e4a5ebc77c5102dcd0
Stuart Kennedy <rav2040@gmail.com>
2020-04-13 16:54:53 +1200

    Add new test for handleRedirectResponse()

---
+++ tests/route-handlers/handle-redirect-response.test.ts
@@ -0,0 +1,101 @@
+import {
+  handleRedirectResponse,
+  handleRedirectResponseWithPreHandlers,
+} from '../../src/route-handlers';
+import { HTTP2_HEADER_STATUS, HTTP2_HEADER_LOCATION, HTTP_STATUS_FOUND } from '../../src/constants';
+
+const mockErrorHandler = jest.fn();
+
+const mockStream = {
+  respond: jest.fn(),
+};
+
+const mockTuftContext: any = {
+  outgoingHeaders: {},
+  setHeader: jest.fn((key, value) => {
+    mockTuftContext.outgoingHeaders[key] = value;
+  }),
+};
+
+describe('handleRedirectResponse()', () => {
+  beforeAll(() => {
+    mockStream.respond.mockClear();
+  });
+
+  test('stream.respond() is called', () => {
+    const responseObj = {
+      status: HTTP_STATUS_FOUND,
+      redirect: '/foo',
+    };
+
+    const result = handleRedirectResponse(
+      responseObj,
+      //@ts-ignore
+      mockStream,
+    );
+
+    const expectedHeaders = {
+      [HTTP2_HEADER_STATUS]: HTTP_STATUS_FOUND,
+      [HTTP2_HEADER_LOCATION]: '/foo',
+    };
+
+    expect(result).toBeUndefined();
+    expect(mockStream.respond).toHaveBeenCalledWith(expectedHeaders, { endStream: true });
+  });
+});
+
+describe('handleRedirectResponseWithPreHandlers()', () => {
+  beforeEach(() => {
+    mockErrorHandler.mockClear();
+    mockTuftContext.outgoingHeaders = {};
+    mockTuftContext.setHeader.mockClear();
+    mockStream.respond.mockClear();
+  });
+
+  test('stream.respond() is called', async () => {
+    const responseObj = {
+      status: HTTP_STATUS_FOUND,
+      redirect: '/foo',
+    };
+    const preHandlers = [() => {}];
+
+    const result = handleRedirectResponseWithPreHandlers(
+      mockErrorHandler,
+      preHandlers,
+      responseObj,
+      //@ts-ignore
+      mockStream,
+      mockTuftContext,
+    );
+
+    await expect(result).resolves.toBeUndefined();
+    expect(mockErrorHandler).not.toHaveBeenCalled();
+    expect(mockTuftContext.setHeader).toHaveBeenCalledWith(HTTP2_HEADER_STATUS, HTTP_STATUS_FOUND);
+    expect(mockTuftContext.setHeader).toHaveBeenCalledWith(HTTP2_HEADER_LOCATION, '/foo');
+    expect(mockStream.respond)
+      .toHaveBeenCalledWith(mockTuftContext.outgoingHeaders, { endStream: true });
+  });
+
+  test('returns an error when a pre-handler throws an error', async () => {
+    const err = Error('pre-handler error');
+    const responseObj = {
+      status: HTTP_STATUS_FOUND,
+      redirect: '/foo',
+    };
+    const preHandlers = [() => { throw err; }];
+
+    const result = handleRedirectResponseWithPreHandlers(
+      mockErrorHandler,
+      preHandlers,
+      responseObj,
+      //@ts-ignore
+      mockStream,
+      mockTuftContext,
+    );
+
+    await expect(result).resolves.toBeUndefined();
+    expect(mockErrorHandler).toHaveBeenCalledWith(err, mockStream, mockTuftContext);
+    expect(mockTuftContext.setHeader).not.toHaveBeenCalled();
+    expect(mockStream.respond).not.toHaveBeenCalled();
+  });
+});
