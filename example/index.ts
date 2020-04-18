4aa8d604b29047964181f5f8318ae87571a74b7f
Stuart Kennedy <rav2040@gmail.com>
2020-04-18 15:08:07 +1200

    Update example to includes plugins

--- example/index.ts
+++ example/index.ts
@@ -1,74 +1,101 @@
 import { createRouteMap } from '../src';
+import { TuftContext } from '../src/context';
+
+function myPlugin() {
+  return (t: TuftContext) => {
+    t.request.foo = 42;
+  };
+}
+
+function myOtherPlugin() {
+  return (t: TuftContext) => {
+    t.request.hello = 'world';
+  };
+}
+
+const app = createRouteMap({
+  plugins: [
+    myPlugin(),
+    myOtherPlugin(),
+  ],
+});
+
+app.set('GET /a', {
+  response: () => {
+    return { status: 204 };
+  },
+});
+
+app.set('GET /b', {
+  response: { status: 204 },
+});
+
+app.set('GET /foo/bar/baz', {
+  response: {
+    status: 200,
+    body: 'AAA',
+  },
+});
+
+app.set('GET /foo/{**}/baz', {
+  response: {
+    status: 200,
+    body: 'BBB',
+  },
+});
+
+app.set('GET /xyz/{id}', {
+  response: () => {
+    return {
+      status: 200,
+    };
+  },
+});
+
+app.set('GET /json', {
+  response: () => {
+    return {
+      body: {
+        hello: 'world',
+      },
+    };
+  },
+});
+
+app.set('GET /a/b/c/d/e/f', {
+  response: {
+    status: 200,
+    body: 'Hello, world!',
+  },
+});
+
+
+app.set('GET /content_type', {
+  response: {
+    status: 200,
+    contentType: 'text',
+    body: 'Hello, world!',
+  },
+});
+
+app.set('GET /request_object', {
+  response: (t) => {
+    return {
+      body: t.request,
+    };
+  },
+});
+
+app.add({
+  path: '/{**}',
+  response: {
+    status: 404,
+  },
+});
+
 
 void async function() {
-  const routes = createRouteMap();
-
-  routes.set('GET /a', {
-    response: () => {
-      return { status: 204 };
-    },
-  });
-
-  routes.set('GET /b', {
-    response: { status: 204 },
-  });
-
-  routes.set('GET /foo/bar/baz', {
-    response: {
-      status: 200,
-      body: 'AAA',
-    },
-  });
-
-  routes.set('GET /foo/{**}/baz', {
-    response: {
-      status: 200,
-      body: 'BBB',
-    },
-  });
-
-  routes.set('GET /xyz/{id}', {
-    response: () => {
-      return {
-        status: 200,
-      };
-    },
-  });
-
-  routes.set('GET /json', {
-    response: () => {
-      return {
-        body: {
-          hello: 'world',
-        },
-      };
-    },
-  });
-
-  routes.set('GET /a/b/c/d/e/f', {
-    response: {
-      status: 200,
-      body: 'Hello, world!',
-    },
-  });
-
-
-  routes.set('GET /content_type', {
-    response: {
-      status: 200,
-      contentType: 'text',
-      body: 'Hello, world!',
-    },
-  });
-
-  routes.add({
-    path: '/{**}',
-    response: {
-      status: 404,
-    },
-  });
-
-  const server = routes.createServer({ port: 3000 });
+  const server = app.createServer({ port: 3000 });
 
   await server.start();
 
