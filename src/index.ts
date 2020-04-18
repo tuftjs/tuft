5ffd2a02dd43175624caa47ea2007d07eaee3529
Stuart Kennedy <rav2040@gmail.com>
2020-04-18 18:08:08 +1200

    Add type exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
+export type { TuftServer, TuftSecureServer } from './server';
+export type { TuftContext, TuftRequest } from './context';
+export type {
+  TuftHandler,
+  TuftPluginHandler,
+  TuftErrorHandler,
+  TuftStreamHandler,
+  TuftResponse,
+  TuftRouteSchema,
+  RouteMap,
+} from './route-map';
+
 export { createRouteMap } from './route-map';
 export { createPromise } from './utils';
+export { cookieParserPlugin } from './plugins/cookie-parser';
+export { bodyParserPlugin } from './plugins/body-parser';
