55f767e9cc87cff9f32499170f91aafd25f2648a
Stuart Kennedy <rav2040@gmail.com>
2020-05-12 13:26:29 +1200

    Modify stream responder to use factory function

--- src/responders/stream.ts
+++ src/responders/stream.ts
@@ -3,29 +3,31 @@ import type { TuftResponse } from '../route-map';
 import { createPromise } from '../utils';
 import { HTTP2_HEADER_STATUS } from '../constants';
 
-export async function streamResponder(
-  response: TuftResponse,
-  stream: ServerHttp2Stream,
-  outgoingHeaders: OutgoingHttpHeaders,
-) {
-  const { writeStream, status } = response;
+export function createStreamResponder() {
+  return async function streamResponder(
+    response: TuftResponse,
+    stream: ServerHttp2Stream,
+    outgoingHeaders: OutgoingHttpHeaders,
+  ) {
+    const { writeStream, status } = response;
 
-  if (typeof writeStream === 'function') {
-    if (status) {
-      outgoingHeaders[HTTP2_HEADER_STATUS] = status;
+    if (typeof writeStream === 'function') {
+      if (status) {
+        outgoingHeaders[HTTP2_HEADER_STATUS] = status;
+      }
+
+      stream.respond(outgoingHeaders);
+
+      await response.writeStream((chunk: any, encoding?: string) => {
+        return createPromise(done => {
+          stream.write(chunk, encoding, done);
+        });
+      });
+
+      stream.end();
+      return;
     }
 
-    stream.respond(outgoingHeaders);
-
-    await response.writeStream((chunk: any, encoding?: string) => {
-      return createPromise(done => {
-        stream.write(chunk, encoding, done);
-      });
-    });
-
-    stream.end();
-    return;
-  }
-
-  return response;
+    return response;
+  };
 }
