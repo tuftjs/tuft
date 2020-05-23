96f53448941e3d4491ff82b297f6538c8dfec780
Stuart Kennedy <rav2040@gmail.com>
2020-05-23 12:59:16 +1200

    Refactor server classes to use base abstract class

--- src/server.ts
+++ src/server.ts
@@ -17,13 +17,8 @@ export type SecureServerOptions = {
   cert?: string | Buffer | Array<string | Buffer>,
 }
 
-/**
- * Creates an instance of Http2Server and adds a listener for the 'stream' event, using the
- * provided handler function.
- */
-
-export class TuftServer extends EventEmitter {
-  readonly #http2Server: Http2Server;
+abstract class TuftServerBase extends EventEmitter {
+  readonly #server: Http2Server | Http2SecureServer;
   readonly #host: string;
   readonly #port: number;
 
@@ -36,12 +31,14 @@ export class TuftServer extends EventEmitter {
   }
 
   constructor(
+    createServer: any,
+    createServerOptions: any,
     handler: (stream: ServerHttp2Stream, headers: IncomingHttpHeaders) => void,
     options: ServerOptions = {},
   ) {
     super();
 
-    const server = createServer();
+    const server = createServer(createServerOptions);
 
     server.on('error', emitError.bind(this));
     server.on('sessionError', emitSessionError.bind(this));
@@ -49,7 +46,7 @@ export class TuftServer extends EventEmitter {
 
     server.on('stream', handler);
 
-    this.#http2Server = server;
+    this.#server = server;
     this.#host = options.host ?? TUFT_SERVER_DEFAULT_HOST;
     this.#port = options.port ?? TUFT_SERVER_DEFAULT_PORT;
   }
@@ -60,7 +57,7 @@ export class TuftServer extends EventEmitter {
 
   async start() {
     await createPromise(done => {
-      this.#http2Server.listen(this.#port, done);
+      this.#server.listen(this.#port, done);
     });
   }
 
@@ -71,7 +68,7 @@ export class TuftServer extends EventEmitter {
 
   async stop() {
     await createPromise(done => {
-      this.#http2Server.close(done);
+      this.#server.close(done);
     });
   }
 
@@ -81,7 +78,7 @@ export class TuftServer extends EventEmitter {
    */
 
   setTimeout(msec?: number, callback?: () => void) {
-    this.#http2Server.setTimeout(msec, callback);
+    this.#server.setTimeout(msec, callback);
     return this;
   }
 
@@ -91,7 +88,21 @@ export class TuftServer extends EventEmitter {
    */
 
   address() {
-    return this.#http2Server.address();
+    return this.#server.address();
+  }
+}
+
+/**
+ * Creates an instance of Http2Server and adds a listener for the 'stream' event, using the
+ * provided handler function.
+ */
+
+export class TuftServer extends TuftServerBase {
+  constructor(
+    handler: (stream: ServerHttp2Stream, headers: IncomingHttpHeaders) => void,
+    options: ServerOptions = {},
+  ) {
+    super(createServer, {}, handler, options);
   }
 }
 
@@ -100,87 +111,23 @@ export class TuftServer extends EventEmitter {
  * provided handler function.
  */
 
-export class TuftSecureServer extends EventEmitter {
-  readonly #http2SecureServer: Http2SecureServer;
-  readonly #host: string;
-  readonly #port: number;
-
-  get host() {
-    return this.#host;
-  }
-
-  get port() {
-    return this.#port;
-  }
-
+export class TuftSecureServer extends TuftServerBase {
   constructor(
     handler: (stream: ServerHttp2Stream, headers: IncomingHttpHeaders) => void,
     options: SecureServerOptions = {},
   ) {
-    super();
-
-    const server = createSecureServer({ key: options.key, cert: options.cert });
-
-    server.on('error', emitError.bind(this));
-    server.on('sessionError', emitSessionError.bind(this));
-    server.on('timeout', emitTimeout.bind(this));
-
-    server.on('stream', handler);
-
-    this.#http2SecureServer = server;
-    this.#host = options.host ?? TUFT_SERVER_DEFAULT_HOST;
-    this.#port = options.port ?? TUFT_SERVER_DEFAULT_PORT;
-  }
-
-  /**
-   * Returns a promise that resolves once the server has started.
-   */
-
-  async start() {
-    await createPromise(done => {
-      this.#http2SecureServer.listen(this.#port, done);
-    });
-  }
-
-  /**
-   * Returns a promise that resolves once all connections are ended and the server has closed, or
-   * rejects with an error if the server was not running.
-   */
-
-  async stop() {
-    await createPromise(done => {
-      this.#http2SecureServer.close(done);
-    });
-  }
-
-  /**
-   * Sets the timeout value in milliseconds for requests to the server. The provided callback is
-   * invoked whenever the timeout value is reached.
-   */
-
-  setTimeout(msec?: number, callback?: () => void) {
-    this.#http2SecureServer.setTimeout(msec, callback);
-    return this;
-  }
-
-  /**
-   * If called while the server is running, returns an object containing data about the current
-   * server.
-   */
-
-  address() {
-    return this.#http2SecureServer.address();
+    super(createSecureServer, { key: options.key, cert: options.cert }, handler, options);
   }
 }
 
-export function emitError(this: TuftServer | TuftSecureServer, err: Error) {
+export function emitError(this: TuftServerBase, err: Error) {
   this.emit('error', err);
 }
 
-export function emitSessionError(this: TuftServer | TuftSecureServer, err: Error) {
+export function emitSessionError(this: TuftServerBase, err: Error) {
   this.emit('sessionError', err);
 }
 
-export function emitTimeout(this: TuftServer | TuftSecureServer) {
+export function emitTimeout(this: TuftServerBase) {
   this.emit('timeout');
 }
