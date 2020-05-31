7e4a981c3704b9816e9f5ca1cd8a18dc66550f2b
Stuart Kennedy <rav2040@gmail.com>
2020-05-31 16:44:50 +1200

    Use default export instead of named export

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
+import type { RouteMapOptions } from './route-map';
+import { TuftRouteMap } from './route-map';
+
+// Type exports
 export type { TuftHandler, TuftPreHandler, TuftResponse, TuftRouteMap } from './route-map';
 export type { TuftContext, TuftRequest } from './context';
 export type { TuftServer, TuftSecureServer } from './server';
-export { createRouteMap } from './route-map';
+
+// Main export
+export default function tuft(options?: RouteMapOptions) {
+  return new TuftRouteMap(options);
+}
+
+// Extensions
 export { createSearchParams } from './pre-handlers/search-params';
 export { createCookieParser } from './pre-handlers/cookie-parser';
 export { createBodyParser } from './pre-handlers/body-parser';
 export { createWriteStreamResponder } from './responders/write-stream';
+
+// Utility functions
 export { createPromise } from './utils';
