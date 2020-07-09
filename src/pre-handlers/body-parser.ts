2b4d573620f7cf3c7c1ea89e1e888ac7c40b9a76
rav2040 <rav2040@gmail.com>
2020-07-09 15:44:27 +1200

    Pre-allocate and copy buffer instead of concatenating existing chunks

--- src/pre-handlers/body-parser.ts
+++ src/pre-handlers/body-parser.ts
@@ -8,12 +8,6 @@ import {
   HTTP2_HEADER_CONTENT_TYPE,
 } from '../constants';
 
-type BodyParserOptions = {
-  text?: boolean | number,
-  json?: boolean | number,
-  urlEncoded?: boolean | number,
-}
-
 const finished = promisify(finishedStream);
 
 /**
@@ -23,115 +17,114 @@ const finished = promisify(finishedStream);
  * the request body type is compatible.
  */
 
-export function createBodyParser({ text, json, urlEncoded }: BodyParserOptions = {}) {
-  let maxTextSize: number;
-  let maxJsonSize: number;
-  let maxUrlEncodedSize: number;
+export function createBodyParser(type: string, maxSize?: number) {
+  let regexp: RegExp;
+  let convertFn: (buf: Buffer) => Buffer | string | object;
 
-  if (typeof text === 'number') {
-    // Convert bodies of type 'text/*' up to the user defined limit.
-    maxTextSize = text === 0 ? Infinity : text;
+  switch (type) {
+    case 'raw':
+      regexp = /^application\/octet-stream/;
+      convertFn = convertRaw;
+      break;
+    case 'text':
+      regexp = /^text\//;
+      convertFn = convertText;
+      break;
+    case 'json':
+      regexp = /^application\/json/;
+      convertFn = convertJson;
+      break;
+    case 'urlEncoded':
+      regexp = /^application\/x-www-form-urlencoded/;
+      convertFn = convertUrlEncoded;
+      break;
   }
 
-  else if (text === true) {
-    // Convert bodies of type 'text/*' up to the default limit.
-    maxTextSize = DEFAULT_MAX_BODY_SIZE;
+  if (!maxSize) {
+    maxSize = DEFAULT_MAX_BODY_SIZE;
   }
 
-  if (typeof json === 'number') {
-    // Convert bodies of type 'application/json' up to the user defined limit.
-    maxJsonSize = json === 0 ? Infinity : json;
-  }
-
-  else if (json === true) {
-    // Convert bodies of type 'application/json' up to the default limit.
-    maxJsonSize = DEFAULT_MAX_BODY_SIZE;
-  }
-
-  if (typeof urlEncoded === 'number') {
-    // Convert bodies of type 'application/x-www-form-urlencoded' up to the user defined limit.
-    maxUrlEncodedSize = urlEncoded === 0 ? Infinity : urlEncoded;
-  }
-
-  else if (urlEncoded === true) {
-    // Convert bodies of type 'application/x-www-form-urlencoded' up to the default limit.
-    maxUrlEncodedSize = DEFAULT_MAX_BODY_SIZE;
+  else if (maxSize <= 0) {
+    maxSize = Infinity;
   }
 
   return async function bodyParser(t: TuftContext) {
     const stream = t[streamSymbol];
+    const { headers } = t.request;
 
-    const chunks: Buffer[] = [];
+    const contentType = headers[HTTP2_HEADER_CONTENT_TYPE];
 
-    // Add each 'chunk' of the request body to the 'chunks' array.
+    if (!contentType || !regexp.test(contentType)) {
+      return;
+    }
+
+    let expectedContentLength: string | number | undefined = headers[HTTP2_HEADER_CONTENT_LENGTH];
+
+    if (!expectedContentLength) {
+      // The 'content-length' header is missing.
+      return {
+        error: 'LENGTH_REQUIRED',
+      };
+    }
+
+    expectedContentLength = parseInt(expectedContentLength, 10);
+
+    if (Number.isNaN(expectedContentLength)) {
+      // The 'content-length' header string does not parse to a valid number.
+      return {
+        error: 'BAD_REQUEST',
+      };
+    }
+
+    const buf = Buffer.allocUnsafe(expectedContentLength);
+    let pos = 0;
+    let isTooLarge = false;
+
     stream.on('data', (chunk: Buffer) => {
-      chunks.push(chunk);
+      const offset = pos + chunk.length;
+
+      if (offset > (expectedContentLength as number)) {
+        isTooLarge = true;
+        return;
+      }
+
+      chunk.copy(buf, pos);
+      pos = offset;
     });
 
     // Wait for the stream to end so that we know all chunks have been added.
     await finished(stream, { writable: false });
 
-    let body: null | Buffer | string | { [key in string | number]: any };
-
-    // If there is no request body, 'body' will remain as 'null'.
-    body = null;
-
-    if (chunks.length > 0) {
-      // There is a request body.
-      const { headers } = t.request;
-
-      if (!headers[HTTP2_HEADER_CONTENT_LENGTH]) {
-        // The 'content-length' header is missing.
-        return {
-          error: 'LENGTH_REQUIRED',
-        };
-      }
-
-      // If there are multiple buffers, concatenate them into a single buffer.
-      body = chunks.length === 1 ? chunks[0] : Buffer.concat(chunks);
-
-      const contentType = headers[HTTP2_HEADER_CONTENT_TYPE];
-
-      if (contentType) {
-        // The 'content-type' header is present.
-        if (maxTextSize && /^text\//.test(contentType)) {
-          // The body should be converted to a string.
-          if (body.length > maxTextSize) {
-            return {
-              error: 'PAYLOAD_TOO_LARGE',
-            };
-          }
-
-          body = body.toString();
-        }
-
-        else if (maxJsonSize && /^application\/json/.test(contentType)) {
-          // The body should be converted to an object.
-          if (body.length > maxJsonSize) {
-            return {
-              error: 'PAYLOAD_TOO_LARGE',
-            };
-          }
-
-          body = JSON.parse(body.toString());
-        }
-
-        else if (maxUrlEncodedSize && /^application\/x-www-form-urlencoded/.test(contentType)) {
-          // The body should be converted to an object.
-          if (body.length > maxUrlEncodedSize) {
-            return {
-              error: 'PAYLOAD_TOO_LARGE',
-            };
-          }
-
-          body = parseUrlEncodedStr(body.toString());
-        }
-      }
+    if (isTooLarge) {
+      return {
+        error: 'PAYLOAD_TOO_LARGE',
+      };
     }
 
-    // Attach the parsed request body to the request object.
-    t.request.body = body;
+    if (pos !== expectedContentLength) {
+      return {
+        error: 'BAD_REQUEST',
+      };
+    }
+
+    t.request.body = convertFn(buf);
   };
+}
+
+function convertRaw(buf: Buffer) {
+  return buf;
+}
+
+function convertText(buf: Buffer) {
+  return buf.toString('utf-8');
+}
+
+function convertJson(buf: Buffer) {
+  return JSON.parse(buf.toString('utf-8'));
+}
+
+function convertUrlEncoded(buf: Buffer) {
+  return parseUrlEncodedStr(buf.toString('utf-8'));
 }
 
 /**
